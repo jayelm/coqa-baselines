4f060d15fedc8e81bab7aa57a8c47bdac490c21a
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2018-10-15 11:09:39 -0700

    Change wordvecs for simple recipe

--- coqa_simple_recipe.sh
+++ coqa_simple_recipe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -rf exp_simple
-python rc/main_simple.py --trainset data/coqa/coqa-train-v1.0-processed.json --devset data/coqa/coqa-dev-v1.0-processed.json --dir exp_simple --embed_file data/wordvecs/crawl-300d-2M.vec --embed_type fasttext --f_history y --cuda false --dialog_batched true --debug true --batch_size 1
+python rc/main_simple.py --trainset data/coqa/coqa-train-v1.0-processed.json --devset data/coqa/coqa-dev-v1.0-processed.json --dir exp_simple --embed_file data/wordvecs/glove.6B.50d.txt --embed_type fasttext --f_history y --cuda false --dialog_batched true --debug true --batch_size 1
