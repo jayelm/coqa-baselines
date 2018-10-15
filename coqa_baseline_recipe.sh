f89446a6bcdb6bd7b51b4be9fbaf6dab0bcf5827
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2018-10-15 13:23:48 -0700

    Add exp dir argument to baseline recipe

--- coqa_baseline_recipe.sh
+++ coqa_baseline_recipe.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python rc/main.py --trainset data/coqa/coqa-train-v1.0-processed.json --devset data/coqa/coqa-dev-v1.0-processed.json --dir exp --embed_file data/wordvecs/crawl-300d-2M.vec --embed_type fasttext --f_history y --dialog_batched true --batch_size 1
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 EXP_DIR"
+    exit 1
+fi
+
+python rc/main.py --trainset data/coqa/coqa-train-v1.0-processed.json --devset data/coqa/coqa-dev-v1.0-processed.json --dir "$1" --embed_file data/wordvecs/crawl-300d-2M.vec --embed_type fasttext --f_history y --dialog_batched true --batch_size 1 --cuda_id 0
