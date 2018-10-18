d5499dd1cb7727430981dbe463be7723b7f42dc5
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2018-10-17 18:07:22 -0700

    Modify run_debug to include coqa vecs

--- run_debug.sh
+++ run_debug.sh
@@ -3,4 +3,6 @@
 # A simple CoQA recipe for debugging without cuda.
 
 rm -rf exp/debug
-python rc/main.py --trainset data/coqa/coqa-train-v1.0-processed.json --devset data/coqa/coqa-dev-v1.0-processed.json --dir exp/debug --embed_file data/wordvecs/glove.6B.50d.txt --embed_type fasttext --f_history y --cuda false --dialog_batched true --debug true --batch_size 1 --num_layers 1 --hidden_size 50 --recency_bias true --use_history_qemb true
+python rc/main.py --trainset data/coqa/coqa-train-v1.0-processed.json --devset data/coqa/coqa-dev-v1.0-processed.json --dir exp/debug --embed_file data/wordvecs/coqa.glove.6B.50d.txt --embed_type fasttext --f_history y --cuda false --debug true \
+    --dialog_batched true --batch_size 1 --num_layers 1 --hidden_size 50 \
+    --recency_bias true --use_history_qemb true
