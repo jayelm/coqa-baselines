611ec1b51395fea15412293d0142ee6dfb792461
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2018-10-31 17:46:26 -0700

    Add option to overwrite arg

--- run_baseline.sh
+++ run_baseline.sh
@@ -5,6 +5,16 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+if [ -d "$1" ]; then
+    read -p "$1 already exists. Remove? [yN] " -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        rm -r $1
+    else
+        exit 0
+    fi
+fi
+
 python rc/main.py --trainset data/coqa/coqa-train-v1.0-processed.json --devset data/coqa/coqa-dev-v1.0-processed.json --embed_file data/wordvecs/coqa.crawl-300d-2M.vec --embed_type fasttext --f_history y \
     --dir "$1" \
     --dialog_batched true --batch_size 2 \
