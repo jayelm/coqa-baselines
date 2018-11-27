a28cceba5ba8977f73d8595a946e205e4588120d
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2018-11-27 01:11:08 -0800

    Add bert preprocessing script

--- data/coqa/gen_drqa_data_wt_heuristics.py
+++ data/coqa/gen_drqa_data_wt_heuristics.py
@@ -11,6 +11,10 @@ import string
 import numpy as np
 from collections import Counter
 from pycorenlp import StanfordCoreNLP
+import sys
+sys.path.append('../../bert/')
+import tokenization as bt
+import pandas as pd
 
 nlp = StanfordCoreNLP('http://localhost:9000')
 UNK = 'unknown'
@@ -34,6 +38,14 @@ def _str(s):
     elif (s.lower() == '-rcb-'):
         s = '}'
     return s
+
+
+def process_bert(text, tokenizer):
+    word, offsets = tokenizer.tokenize(text, return_offsets=True)
+    return {
+        'word': word,
+        'offsets': offsets
+    }
 
 
 def process(text):
@@ -125,7 +137,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_file', '-d', type=str, required=True)
     parser.add_argument('--output_file', '-o', type=str, required=True)
+    parser.add_argument('--bert', '-b', action='store_true', default=False,
+                        help='Use bert tokenization')
     args = parser.parse_args()
+
+    if args.bert:
+        tokenizer = bt.BasicTokenizer(do_lower_case=False)
+        _process = lambda x: process_bert(x, tokenizer)
+    else:
+        _process = process
 
     with open(args.data_file, 'r') as f:
         dataset = json.load(f)
@@ -133,20 +153,29 @@ if __name__ == '__main__':
     f1_scores = []
     data = []
     start_time = time.time()
+    diag_rows = []
+    print(','.join(['gold_answer', 'gold_rationale', 'span_reconstructed_answer', 'f1']))
     for i, datum in enumerate(dataset['data']):
         if i % 10 == 0:
             print('processing %d / %d (used_time = %.2fs)...' %
                   (i, len(dataset['data']), time.time() - start_time))
         context_str = datum['story']
+        if args.bert:
+            context_str = tokenizer._run_strip_accents(context_str)
+            context_str, c_breaks = tokenizer._clean_text(context_str, return_breaks=True)
         _datum = {'context': context_str,
-                  'source': datum['source'],
-                  'id': datum['id'],
-                  'filename': datum['filename']}
-        _datum['annotated_context'] = process(context_str)
+              'source': datum['source'],
+              'id': datum['id'],
+              'filename': datum['filename']}
+        _datum['annotated_context'] = _process(context_str)
         _datum['qas'] = []
         for token in [UNK, YES, NO]:
-            _datum['context'] += token
+            if args.bert:
+                _datum['context'] += ' ' + token
+            else:
+                _datum['context'] += token
             _datum['annotated_context']['word'].append(token)
+            # TODO: make sure this actually works now that there are spaces.
             _datum['annotated_context']['offsets'].append(
                 (len(_datum['context']) - len(token), len(_datum['context'])))
         assert len(datum['questions']) == len(datum['answers'])
@@ -164,21 +193,48 @@ if __name__ == '__main__':
         for question, answer in zip(datum['questions'], datum['answers']):
             assert question['turn_id'] == answer['turn_id']
             idx = question['turn_id']
+            if args.bert:
+                question['input_text'] = tokenizer._run_strip_accents(question['input_text'])
+                question['input_text'], q_breaks = tokenizer._clean_text(question['input_text'], return_breaks=True)
+                answer['input_text'] = tokenizer._run_strip_accents(answer['input_text'])
+                answer['input_text'], a_breaks = tokenizer._clean_text(answer['input_text'], return_breaks=True)
+                answer['span_text'] = tokenizer._run_strip_accents(answer['span_text'])
+                answer['span_text'], a_breaks = tokenizer._clean_text(answer['span_text'], return_breaks=True)
             _qas = {'turn_id': idx,
                     'question': question['input_text'],
                     'answer': answer['input_text']}
             if idx in additional_answers:
                 _qas['additional_answers'] = additional_answers[idx]
 
-            _qas['annotated_question'] = process(question['input_text'])
-            _qas['annotated_answer'] = process(answer['input_text'])
-            _qas['answer_span_start'] = answer['span_start']
-            _qas['answer_span_end'] = answer['span_end']
+            _qas['annotated_question'] = _process(question['input_text'])
+            _qas['annotated_answer'] = _process(answer['input_text'])
+            true_span_start = answer['span_start']
+            true_span_end = answer['span_end']
+            if args.bert:  # Fix spans if bert preprocessing changes locations
+                for b in c_breaks:
+                    if true_span_start > b:
+                        true_span_start -= 1
+                    if true_span_end > b:
+                        true_span_end -= 1
+            _qas['answer_span_start'] = true_span_start
+            _qas['answer_span_end'] = true_span_end
             _qas['answer_span'], _qas['f1'] = find_span(_datum['context'], _datum['annotated_context']['offsets'],
-                                                        _qas['answer'], answer['span_start'], answer['span_end'])
+                                                        _qas['answer'], true_span_start, true_span_end)
+            diagnostic_row = (_qas['answer'],
+                              _datum['context'][true_span_start:true_span_end],
+                              _datum['annotated_context']['word'][_qas['answer_span'][0]:_qas['answer_span'][1]+1],
+                              _qas['f1'])
+            if args.bert:
+                if _datum['context'][true_span_start:true_span_end] != answer['span_text']:
+                    if (('bad_turn' not in answer) or not answer['bad_turn']) and answer['span_text'] != 'unknown':
+                        print(diagnostic_row)
+            diag_rows.append(diagnostic_row)
             _datum['qas'].append(_qas)
             f1_scores.append(_qas['f1'])
         data.append(_datum)
+
+    df = pd.DataFrame(diag_rows, columns=['gold_answer', 'gold_rationale', 'span_reconstructed_answer', 'f1'])
+    df.to_csv('./diagnostic.csv', index=False)
 
     dataset['data'] = data
     with open(args.output_file, 'w') as output_file:
